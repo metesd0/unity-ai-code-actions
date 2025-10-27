bd33d035cc268ba9ff100f1004a038487903093a
AI Code Actions <support@aicodeactions.dev>
2025-10-27 21:32:58 +0300

    fix: Add missing using directive for IModelProvider in ReAct files
    ReActAgent.cs and UnityAgentTools.ReAct.cs were missing:
    using AICodeActions.Providers;
    
    This caused CS0246 errors (type not found) for IModelProvider interface.
    Fixed by adding the missing using directive to both files.

--- Editor/AICodeActions/Core/ReActAgent.cs
+++ Editor/AICodeActions/Core/ReActAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using AICodeActions.Providers;
 
 namespace AICodeActions.Core
 {
--- Editor/AICodeActions/Core/UnityAgentTools.ReAct.cs
+++ Editor/AICodeActions/Core/UnityAgentTools.ReAct.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using AICodeActions.Providers;
 
 namespace AICodeActions.Core
 {
